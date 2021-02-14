117e85e987e015e09263ef6078753d43f8311e94
selincaglarr <69199076+selincaglarr@users.noreply.github.com>
2021-02-14 15:48:41 +0300

    Add files via upload

---
+++ routes/routes/auth.js
@@ -0,0 +1,32 @@
+const bcrypt = require("bcryptjs");
+const Joi = require("joi");
+const { User } = require("../models/user");
+const mongoose = require("mongoose");
+const router = require("express").Router();
+
+router.post("/", async (req, res) => {
+  //LETS VALIDATE THE DATA BEFORE MAKE A USER
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email.");
+
+  //Hash the password 10 is complexcity
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid password.");
+
+  res.send(user);
+});
+
+function validate(req) {
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  });
+  return schema.validate(req);
+}
+module.exports = router;
